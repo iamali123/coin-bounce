460704a222d801ff55a641945905a7e528d37957
iamali123 <iamali62426@gmail.com>
2024-04-25 17:54:04 +0400

    blog crud

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,21 @@ const dbConnect = require("./database/index");
 const app = express();
 const {PORT} = require("./config/index");
 const router = require("./routes/index");
+const errorHandler = require("./middlewares/errorHandler");
+const cookieParser = require("cookie-parser");
 
 dbConnect();
+
+app.use(cookieParser());
+app.use(express.json());
 
 app.use(router);
 
 // app.get("/", (req, res) => {
 //   res.send("Hello World1123!");
 // });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
