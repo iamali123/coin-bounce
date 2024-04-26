54dc3481f518642ca3990c4c21e427a6c8ebe397
iamali123 <iamali62426@gmail.com>
2024-04-26 10:51:06 +0400

    frontend apis implementation

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,40 @@
 const express = require("express");
 const dbConnect = require("./database/index");
-const app = express();
-const {PORT} = require("./config/index");
+const { PORT } = require("./config/index");
 const router = require("./routes/index");
 const errorHandler = require("./middlewares/errorHandler");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
-dbConnect();
+const corsOptions = {
+  credentials: true,
+  origin: ["http://localhost:3000"],
+};
+
+const app = express();
 
 app.use(cookieParser());
-app.use(express.json());
-app.use("/storage", express.static("storage"));
+
+app.use(cors(corsOptions));
+
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       return callback(null, true);
+//     },
+//     optionsSuccessStatus: 200,
+//     credentials: true,
+//   })
+// );
+
+app.use(express.json({ limit: "50mb" }));
 
 app.use(router);
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World1123!");
-// });
+dbConnect();
+
+app.use("/storage", express.static("storage"));
 
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
-});
+app.listen(PORT, console.log(`Backend is running on port: ${PORT}`));
